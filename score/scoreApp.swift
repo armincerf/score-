be8dc2dc09427e8f3e9a08db5b9ed1aeee5ef938
Alex Davis <alex@whimsical.com>
2025-11-29 20:35:55 +0100

    Initial Commit

---
+++ score/scoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  scoreApp.swift
+//  score
+//
+//  Created by Alex Davis on 29/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct scoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
